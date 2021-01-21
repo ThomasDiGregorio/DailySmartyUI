547d70d5b165fda6e719332ab7c62a0af9be155c
ThomasDiGregorio <digregoriothomas@yahoo.com>
2021-01-21 12:41:29 -0500

    Created SearchBar and Recent Posts

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import LogoSrc from "../../static/assets/ds_circle_logo.png"
 
 class Logo extends Component {
     render() {
         return (
             <div className="logo-main">
-                <img alt="daily smarty ui image logo big" src="/assets/ds_circle_logo.png" />
+                <img alt="altLogo" src={LogoSrc} />
             </div>
         )
     }
