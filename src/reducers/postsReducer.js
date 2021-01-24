85a1383cef6e6b4b96b7bc1894124220abec3930
ThomasDiGregorio <digregoriothomas@yahoo.com>
2021-01-23 19:50:11 -0500

    put together resultposts component

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,23 +1,20 @@
-import {
-    SET_RECENT_POSTS
-} from '../actions/types';
-import recentPosts from '../components/recentPosts';
+import { SET_RECENT_POSTS } from "../actions/types";
 
 const INIT_STATE = {
-    posts: [],
-    recentPosts: []
-}
+  posts: [],
+  recentPosts: [],
+};
 
-export default function(state = INIT_STATE, action) {
-    switch (action.type) {
-        case SET_RECENT_POSTS:
-            const recentPosts = action.payload;
-            return {
-                    ...state, 
-                    recentPosts
-                }
-    
-        default:
-            return state;
-    }
+export default function (state = INIT_STATE, action) {
+  switch (action.type) {
+    case SET_RECENT_POSTS:
+      const recentPosts = action.payload;
+      return {
+        ...state,
+        recentPosts,
+      };
+
+    default:
+      return state;
+  }
 }
