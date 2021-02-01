f89bf5d0c763d5faf016e0a04d57cc8b169a82db
ThomasDiGregorio <digregoriothomas@yahoo.com>
2021-02-01 18:53:00 -0500

    mapped state to props in results posts component

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
-import LogoSrc from "../../static/assets/ds_circle_logo.png"
+import React, { Component } from "react";
+import LogoSrc from "../../static/assets/ds_circle_logo.png";
+
+import { Link } from "react-router-dom";
 
 class Logo extends Component {
-    render() {
-        const size = {
-            height: this.props.size ? this.props.size : 105,
-            width: this.props.size ? this.props.size : 105,
-        }
-        return (
-            <div className="logo-main">
-                <img style={size} alt="daily smarty ui image logo big" src={LogoSrc} />
-            </div>
-        )
-    }
+  render() {
+    const size = {
+      height: this.props.size ? this.props.size : 105,
+      width: this.props.size ? this.props.size : 105,
+    };
+    return (
+      <div className="logo-main">
+        <Link to="/">
+          <img
+            style={size}
+            alt="daily smarty ui image logo big"
+            src={LogoSrc}
+          />
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default Logo;
